a3b9c809f9d65141af9b1b9cb9760a7a917e1a1d
RxL-Nishant-Kumar <nishant.kumar@rxlogix.com>
2022-08-08 18:02:40 +0530

    multiple_small_additions

--- dashboard_script.js
+++ dashboard_script.js
@@ -3,4 +3,18 @@ $(document).ready(function () {
         var selText = $(this).text();
         $(this).parents('.btn-group').find('.dropdown-toggle').html(selText + ' <span class="caret"></span>');
     });
+
+    $("#shareThisTopic").text($("#currentTopicName").text())
+    $("#deleteThisTopic").text($("#currentTopicName").text())
+
+
+    // $("#editCardButton").click(function (e) {
+    //     $("#newTopicName").attr('placeholder', $("#currentTopicName").text())
+    //     $("#currentTopicName").hide();
+    //     $("#newTopicName").show();
+    //     $("editCardFooter").
+
+    // });
+
+
 }); 
